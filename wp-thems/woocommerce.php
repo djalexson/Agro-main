4a06bc48b13a886373819c9164ea2235dab32523
djalexson <djalexson@gmail.com>
2023-05-08 22:24:55 +0500

    wp wc

--- wp-thems/woocommerce.php
+++ wp-thems/woocommerce.php
@@ -14,27 +14,31 @@
  */
 
 get_header();
+
+get_template_part("temlates_blocks/header_gg")
 ?>
-14
-<div id="content" class="site-content container py-5 mt-4">
-  <div id="primary" class="content-area">
-
-    <!-- Hook to add something nice -->
+		
+		<!-- Hook to add something nice -->
     <?php bs_after_primary(); ?>
-
-    <main id="main" class="site-main">
-
-      <!-- Breadcrumb -->
-      <?php woocommerce_breadcrumb(); ?>
-      <div class="row">
-        <div class="col">
-          <?php woocommerce_content(); ?>
+		
+    <main>
+			<?php if(is_singular( 'product' )): ?>
+				
+				<?php woocommerce_breadcrumb(); ?>
+				<!-- Breadcrumb -->
+				<?php endif; ?>
+				<div class="row">
+					<div class="col">
+          <?php if ( is_singular( 'product' ) ) {
+						woocommerce_content();
+					}else{
+wc_get_template( 'archive-product.php' );
+} ?>
         </div>
         <!-- sidebar -->
            <!-- row -->
       </div>
     </main><!-- #main -->
-  </div><!-- #primary -->
-</div><!-- #content -->
+
 <?php
 get_footer();
